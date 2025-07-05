30f8d0cdaa42e3054e8a5bd3146fd35cd2514f43
renanjfa <renanjusan@gmail.com>
2025-07-04 21:08:49 -0300

    Planejamento main.c e Makefile finalizado

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    // criacao de strings para dirEntrada, dirSaida, arquivoGeo, arquivoQry, arquivoVia
+    
+    // loop para leitura dos parametros
+    
+    // processa geo
+        // leitura geo e colocar quadras na smutreap??
+
+    // processa via
+        // leitura via e inserir em graph
+
+
+    // inicializacao de registradores de endereco, percurso
+
+    // processa qry 
+        // leitura e operacoes
+
+}
